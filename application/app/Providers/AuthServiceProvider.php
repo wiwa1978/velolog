2c8cbaf0f85a00a983efc9d137e9b091e55e163d
Drew Rowan <drew.rowan@mvfglobal.com>
2020-05-23 20:07:01 +0100

    passport back in - works locally but not online

--- application/app/Providers/AuthServiceProvider.php
+++ application/app/Providers/AuthServiceProvider.php
@@ -26,8 +26,8 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-        // Passport::routes();
-        // Passport::tokensExpireIn(Carbon::now()->addDays(1));
-        // Passport::refreshTokensExpireIn(Carbon::now()->addDays(10));
+        Passport::routes();
+        Passport::tokensExpireIn(Carbon::now()->addDays(1));
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(10));
     }
 }
