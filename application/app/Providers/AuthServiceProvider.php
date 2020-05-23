7552c9af97e0502617db5867a87377314b40a318
Drew Rowan <drew.rowan@mvfglobal.com>
2020-05-23 20:10:38 +0100

    removed passport... again

--- application/app/Providers/AuthServiceProvider.php
+++ application/app/Providers/AuthServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Laravel\Passport\Passport;
 use Carbon\Carbon;
 
 class AuthServiceProvider extends ServiceProvider
@@ -26,8 +25,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-        Passport::routes();
-        Passport::tokensExpireIn(Carbon::now()->addDays(1));
-        Passport::refreshTokensExpireIn(Carbon::now()->addDays(10));
     }
 }
