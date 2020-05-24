0ebe85af4e40d3abd263f5c6bb770c266919e682
Drew Rowan <drew.rowan@mvfglobal.com>
2020-05-24 18:34:41 +0100

    css sucks! fixed strava box float issue

--- application/resources/views/index.blade.php
+++ application/resources/views/index.blade.php
@@ -76,16 +76,10 @@
                                 </div>
 
                                 <div class="form-group row">
-                                    <label for="connect-strava" class="col-md-4 col-form-label text-md-right pt-4">{{ __('Connect Strava?') }}</label>
+                                    <label for="connect-strava" class="col-md-4 col-form-label text-md-right pt-4 w-25">{{ __('Connect Strava?') }}</label>
 
-                                    <div class="col-md-6">
-                                        <input type="checkbox" class="form-check-input ml-0 mt-4" id="connect-strava" name="connectstrava">
-
-                                        @error('password')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
+                                    <div class="form-check">
+                                        <input type="checkbox" class="form-control form-check-input ml-0 mt-4" id="connect-strava" name="connectstrava" />
                                     </div>
                                 </div>
 
