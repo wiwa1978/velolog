d7716b15bfd0081320ce4b81c29866738400198e
Drew Rowan <drew.rowan@mvfglobal.com>
2020-05-27 21:55:53 +0100

    added in google analytics

--- application/public/js/scripts.js
+++ application/public/js/scripts.js
@@ -2,6 +2,14 @@ $(document).ready(function(){
     if (Cookies.get('laravel_cookie_consent')) {
         $('#cookie-consent-container').remove();
         // load ads
+
+        // load ga
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-167849652-1');
+
     } else {
         // remove ads
     }
